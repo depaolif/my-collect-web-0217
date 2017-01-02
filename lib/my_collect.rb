cf4539e730e508686a63347e55ce55e695a73837
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-01-02 22:19:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
 
 
+# require 'pry'
+
+def my_collect(array)
+	i = 0
+	narr = Array.new(array.length)
+	while i < narr.length
+		narr[i] = yield array[i]
+		i += 1
+	end
+	narr
+end
+
+# col = [1,2,3]
+# neu = my_collect(col) {|e| e + 1}
+
+# puts col
+# puts neu
